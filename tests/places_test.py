38a00ac8a7a21996494cae93178826276a0d067a
Rory Sullivan <codingrory@gmail.com>
2020-04-18 11:24:16 +0100

    Updates to test.

--- tests/places_test.py
+++ tests/places_test.py
@@ -1,21 +1,41 @@
 """Tests for our places functionality."""
 import unittest
 import os
+import pathlib
 
-from dryrock.places import Place, create_csv
+from dryrock import places
 
 
 class TestPlace(unittest.TestCase):
     """Tests for Place class."""
 
     def setUp(self):
-        self.dublin = Place(
+        self.dublin = places.Place(
             "Dublin",
             "Ireland/Leinster/Dublin",
             "https://www.yr.no/en/forecast/daily-table/2-2964574",
         )
 
     def test_attributes(self):
+        """Test if attributes are set correctly."""
+
+        value = self.dublin.name
+        expected_value = "Cork"
+        self.assertEqual(value, expected_value)
+
+        value = self.dublin.location
+        expected_value = "Ireland/Leinster/Dublin"
+        self.assertEqual(value, expected_value)
+
+        value = self.dublin.yr_url
+        expected_value = "http://www.yr.no/place/Ireland/Leinster/Dublin/"
+        self.assertEqual(value, expected_value)
+
+        value = self.dublin.yr_link
+        expected_value = "https://www.yr.no/en/forecast/daily-table/2-2964574"
+        self.assertEqual(value, expected_value)
+
+    def test_attributes2(self):
         """Test if attributes are set correctly."""
 
         value = self.dublin.name
@@ -35,17 +55,23 @@ class TestPlace(unittest.TestCase):
         self.assertEqual(value, expected_value)
 
 
-class TestPlaces(unittest.TestCase):
-    """Tests for creating places.csv, adding getting and removing places."""
+# class TestPlaces(unittest.TestCase):
+#     """Tests for creating places.csv, adding getting and removing places."""
 
-    INPUT_PATH = "./tests/input/"
-    FILE_PATH = INPUT_PATH.joinpath("places.csv")
+#     @classmethod
+#     def setUpClass(cls):
+#         places.INPUT_PATH = pathlib.Path("./tests/input/")
+#         places.FILE_PATH = places.INPUT_PATH.joinpath("places.csv")
 
-    def tearDown(self):
-        os.remove(FILE_PATH)
-        os.rmdir(INPUT_PATH)
+#     def tearDown(self):
+#         os.remove(places.FILE_PATH)
+#         os.rmdir(places.INPUT_PATH)
 
-    def test_create_csv(self):
-        create_csv()
+#     def test_create_csv(self):
+#         places.create_csv()
 
-        self.assertTrue(FILE_PATH.exists())
+#         self.assertFalse(places.FILE_PATH.exists())
+
+
+# if __name__ == "__main__":
+#     unittest.main()
