b50995a4ba3e3672a0a1605d790b2b7b39bc063f
Rory Sullivan <codingrory@gmail.com>
2020-04-18 11:31:07 +0100

    Updates to places tests.

--- tests/places_test.py
+++ tests/places_test.py
@@ -20,25 +20,6 @@ class TestPlace(unittest.TestCase):
         """Test if attributes are set correctly."""
 
         value = self.dublin.name
-        expected_value = "Cork"
-        self.assertEqual(value, expected_value)
-
-        value = self.dublin.location
-        expected_value = "Ireland/Leinster/Dublin"
-        self.assertEqual(value, expected_value)
-
-        value = self.dublin.yr_url
-        expected_value = "http://www.yr.no/place/Ireland/Leinster/Dublin/"
-        self.assertEqual(value, expected_value)
-
-        value = self.dublin.yr_link
-        expected_value = "https://www.yr.no/en/forecast/daily-table/2-2964574"
-        self.assertEqual(value, expected_value)
-
-    def test_attributes2(self):
-        """Test if attributes are set correctly."""
-
-        value = self.dublin.name
         expected_value = "Dublin"
         self.assertEqual(value, expected_value)
 
@@ -55,23 +36,23 @@ class TestPlace(unittest.TestCase):
         self.assertEqual(value, expected_value)
 
 
-# class TestPlaces(unittest.TestCase):
-#     """Tests for creating places.csv, adding getting and removing places."""
+class TestPlaces(unittest.TestCase):
+    """Tests for creating places.csv, adding getting and removing places."""
 
-#     @classmethod
-#     def setUpClass(cls):
-#         places.INPUT_PATH = pathlib.Path("./tests/input/")
-#         places.FILE_PATH = places.INPUT_PATH.joinpath("places.csv")
+    @classmethod
+    def setUpClass(cls):
+        places.INPUT_PATH = pathlib.Path("./tests/input/")
+        places.FILE_PATH = places.INPUT_PATH.joinpath("places.csv")
 
-#     def tearDown(self):
-#         os.remove(places.FILE_PATH)
-#         os.rmdir(places.INPUT_PATH)
+    def tearDown(self):
+        os.remove(places.FILE_PATH)
+        os.rmdir(places.INPUT_PATH)
 
-#     def test_create_csv(self):
-#         places.create_csv()
+    def test_create_csv(self):
+        places.create_csv()
 
-#         self.assertFalse(places.FILE_PATH.exists())
+        self.assertTrue(places.FILE_PATH.exists())
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
