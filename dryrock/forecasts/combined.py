9011aafa15a81a33de6d6d19eadc9aca84978e59
Rory Sullivan <codingrory@gmail.com>
2020-06-25 08:57:56 +0100

    Add option to only render instead of fetching data  and rendering.

--- dryrock/forecasts/combined.py
+++ dryrock/forecasts/combined.py
@@ -31,6 +31,29 @@ class WeatherData:
             self.yr_data_for_places[place.name] = yr_data_for_place
 
 
+class WeatherDataNoUpdate:
+    """ Class for gathering and storing all of our weather data. """
+
+    def __init__(self, date: dt.datetime, places: list, output_path):
+
+        self.date = date
+        self.places = places
+
+        # Collect information from Yr.
+        self.yr_data_for_places = {}
+
+        for place in self.places:
+            yr_data_for_place = YrData(date, place, output_path)
+
+            # ------------------------------------------------------------------
+            # Set which forecast we want to use.
+            # ------------------------------------------------------------------
+            # yr_data_for_place.update_long_range_forecast()
+            # yr_data_for_place.update_hour_by_hour_forecast()
+
+            self.yr_data_for_places[place.name] = yr_data_for_place
+
+
 # For testing our code.
 # if __name__ == "__main__":
 #     from general_classes import Place
