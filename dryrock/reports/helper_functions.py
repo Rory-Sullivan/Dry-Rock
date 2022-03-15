3a8b262afb5b7a77c2753fee36cca5a21556939a
Rory Sullivan <codingrory@gmail.com>
2022-03-15 08:50:38 +0000

    Updates name of wind direction unit

--- dryrock/reports/helper_functions.py
+++ dryrock/reports/helper_functions.py
@@ -6,7 +6,7 @@ from metno_locationforecast.data_containers import Interval, Variable
 
 def cardinal_name_of(direction_variable: Variable) -> str:
     assert direction_variable.name == "wind_from_direction"
-    assert direction_variable.units == "degrees"
+    assert direction_variable.units == "degree"
 
     value = direction_variable.value
     if (337.5 <= value <= 360) or (0 <= value < 22.5):
