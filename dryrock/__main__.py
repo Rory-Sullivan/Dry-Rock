6660162525831647475bea11ce7a0f62bdde09ef
Rory Sullivan <codingrory@gmail.com>
2024-12-05 17:44:57 -0800

    Adds render only option and updates logging
    Adds '--render-only' as an input flag to only run the rendering steps
    without updating any weather data.
    
    Updates logging output.

--- dryrock/__main__.py
+++ dryrock/__main__.py
@@ -2,6 +2,7 @@
 
 import logging
 import pathlib
+import sys
 from typing import List
 
 from metno_locationforecast import Forecast
@@ -13,6 +14,7 @@ from .reports.render_html import render_html_pages
 
 if __name__ == "__main__":
 
+    # Create logger
     logger = logging.getLogger(__name__)
     logger.setLevel(LOGGING_LEVEL)
     file_handler = logging.FileHandler("dryrock.log")
@@ -21,16 +23,25 @@ if __name__ == "__main__":
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
-    output_path = pathlib.Path(OUTPUT_PATH)
+    # Check for render only mode (passed as a command line parameter)
+    render_only = False
+    if "--render-only" in sys.argv:
+        render_only = True
+        logger.warning("Running in render only mode")
 
+    # Output path
+    output_path = pathlib.Path(OUTPUT_PATH)
+    logger.info(f"Outputting files to: {output_path}")
     if not output_path.is_dir():
+        logger.debug("Output file path does not exist, creating output file path")
         output_path.mkdir(parents=True)
 
-    logger.info("Starting data collection")
-
+    # Load areas from areas file
+    logger.info("Loading areas")
     areas = get_areas(AREAS_FILE)
-    logger.debug("Got all areas")
+    logger.debug("Loaded areas")
 
+    logger.info("Creating forecasts")
     area_forecasts: List[List[Forecast]] = []
     for area in areas:
         place_forecasts: List[Forecast] = []
@@ -38,25 +49,38 @@ if __name__ == "__main__":
             place_forecasts.append(Forecast(place))
 
         area_forecasts.append(place_forecasts)
+    logger.debug("Created areas")
 
-    for area_forecast in area_forecasts:
-        for place_forecast in area_forecast:
-            status = place_forecast.update()
-            if status == "Data-Modified":
-                logger.debug(f"Forecast data for {place_forecast.place.name} updated")
-            if hasattr(place_forecast, "response") and (
-                status_code := place_forecast.response.status_code
-            ) not in {
-                200,
-                304,
-            }:
-                logger.warning(
-                    f"Received {status_code} response for request to update {place_forecast.place.name} data"  # noqa: E501
-                )
+    if not render_only:
+        # Check for updated weather data
+        logger.info("Updating forecasts")
+        for area_forecast in area_forecasts:
+            for place_forecast in area_forecast:
+                status = place_forecast.update()
+                if status == "Data-Modified":
+                    logger.debug(f"Forecast data for {place_forecast.place.name} updated")
+                if hasattr(place_forecast, "response") and (
+                    status_code := place_forecast.response.status_code
+                ) not in {
+                    200,
+                    304,
+                }:
+                    logger.warning(
+                        f"Received {status_code} response for request to update {place_forecast.place.name} data"  # noqa: E501
+                    )
+        logger.debug("Updated forecasts")
+    else:
+        # Load weather data from files
+        logger.info("Loading forecasts from existing data")
+        for area_forecast in area_forecasts:
+            for place_forecast in area_forecast:
+                place_forecast.load()
+        logger.debug("Loaded forecasts from existing data")
 
-    logger.debug("Updated all forecasts")
-
+    # Render HTML pages
+    logger.info("Rendering pages")
     render_html_pages(areas, area_forecasts)
     copy_index()
+    logger.debug("Rendered pages")
 
-    logger.info("Completed update")
+    logger.info("Complete")
